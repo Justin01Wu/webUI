9ccfae13f5e9768e37ecc6e9340a0796c6bf8e66
Justin01Wu <justin01.wu@gmail.com>
2015-02-16 11:57:45 -0500

    add ng-repeat sample

---
+++ src/main/webapp/angularJs/ng-repeat/ng-repeat.js
@@ -0,0 +1,54 @@
+function customersController($scope) {
+
+	var mockDataForThisTest = [ {
+		id : 1,
+		name : "Justin Wu",
+		country : "USA"
+	}, {
+		id : 2,
+		name : "Rita Wang",
+		country : "Canada"
+	}, {
+		id : 3,
+		name : "Yan",
+		country : "China"
+	}, {
+		id : 4,
+		name : "Yan",
+		country : "China"
+	}, {
+		id : 5,
+		name : "Yan",
+		country : "China"
+	}, {
+		id : 6,
+		name : "Yan",
+		country : "China"
+	}, {
+		id : 7,
+		name : "Yan",
+		country : "China"
+	}, {
+		id : 8,
+		name : "Yan",
+		country : "China"
+	}, {
+		id : 9,
+		name : "Yan",
+		country : "China"
+	}, {
+		id : 10,
+		name : "Yan",
+		country : "China"
+	}, {
+		id : 11,
+		name : "Yan",
+		country : "China"
+	}, {
+		id : 12,
+		name : "Yan",
+		country : "China"
+	} ];
+
+	$scope.people = mockDataForThisTest;
+}
