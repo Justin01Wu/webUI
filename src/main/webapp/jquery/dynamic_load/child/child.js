02ff887a197209d43beda20aa102caa7064f2284
Justin Wu <justin01.wu@gmail.com>
2015-06-11 10:02:34 -0400

    improve dynamic loading sample

---
+++ src/main/webapp/jquery/dynamic_load/child/child.js
@@ -0,0 +1,4 @@
+console.log("child script is running...");
+
+var info = document.getElementById('infoLabel');
+info.innerHTML = info.innerHTML + ' <br/> child script is running...';
---
+++ src/main/webapp/jquery/dynamic_load/child/loading.js
@@ -0,0 +1,32 @@
+/*
+ * dependents on jQuery
+ */
+
+(function($, window, document, undefined) {
+
+	function getCurrentPath() {
+		// how to get current path for running JS code
+		// this skill comes from
+		// http://stackoverflow.com/questions/2255689/how-to-get-the-file-path-of-the-currently-executing-javascript-code
+		var scripts = document.getElementsByTagName("script");
+		var currentFile = scripts[scripts.length - 1].src;
+		var currentPath = currentFile.substr(0, currentFile.lastIndexOf('/'))	+ "/";
+		return currentPath;
+	}
+
+	var childHtml = getCurrentPath() + "child.html";
+
+	console.log(childHtml);
+	
+	var info = document.getElementById('infoLabel');
+	info.innerHTML = info.innerHTML +' <br/> loading file:' + childHtml;  
+
+	$.get(childHtml, function(data) {
+		$(".result").html(data);
+		//alert("Load was performed.");
+		var info = document.getElementById('infoLabel');
+		info.innerHTML = info.innerHTML +' <br/> loaded file:' + childHtml;  
+
+	});
+
+})(jQuery, window, document);
