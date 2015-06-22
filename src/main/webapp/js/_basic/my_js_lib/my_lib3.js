1bb88d8a31a916c3f9419c23ad31330f370059a9
Justin Wu <justin01.wu@gmail.com>
2015-06-22 15:41:20 -0400

    add my js lib3

---
+++ src/main/webapp/js/_basic/my_js_lib/my_lib3.js
@@ -0,0 +1,53 @@
+// define anonymous object in lib
+(function(window) {
+
+	"use strict"
+
+	/*
+	 * MyJs3 CLASS DEFINITION ======================
+	 */
+	
+	if(window.MyJs3){
+		if(window.console){
+			console.log("MyJs3 has been registered, skip now");
+		}
+		return;
+	}
+	
+	var MyJs3 = function(options) {
+		this.options = options
+	};
+
+	MyJs3.prototype = {
+
+		constructor : MyJs3,
+
+		toggle : function() {
+		},
+
+		show : function() {
+			var that = this;
+			if(that.options.id){
+				var target = document.getElementById(that.options.id);
+				if(target){
+					alert('find target  Element by Id:' + that.options.id);	
+				}				
+			}
+
+		}
+	}
+	
+	window.MyJs3= MyJs3;
+
+	/*
+	 * MODAL PRIVATE METHODS =====================
+	 */
+
+	function _getCurrentPath() {
+		var scripts = document.getElementsByTagName("script");
+		var currentFile = scripts[scripts.length - 1].src;
+		var currentPath = currentFile.substr(0, currentFile.lastIndexOf('/')) + "/";
+		return currentPath;
+	}
+
+})(window);
