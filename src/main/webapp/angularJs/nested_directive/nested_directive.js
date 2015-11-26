3e5c720ffe8b7bc03f398ef693ac4332428efce7
Justin Wu <justin01.wu@gmail.com>
2015-11-26 17:03:58 -0500

    improve nested directive sample

--- src/main/webapp/angularJs/nested_directive/nested_directive.js
+++ src/main/webapp/angularJs/nested_directive/nested_directive.js
@@ -8,11 +8,16 @@ app.controller('MainCtrl', function ($scope) {
 app.directive('outerDirective', function () {
     return{
         scope: {},
-        restrict: 'AE',
-        controller: function ($scope, $compile, $http) { //dependencies can be injected
+        transclude: true,  // allow to include child content
+        template: "<fieldset>" +
+            "<legend>outer directive</legend>"+            
+            " <div ng-transclude/> "+
+            "</fieldset>",
+        restrict: 'E',
+        controller: function ($scope) { 
             // $scope is the appropriate scope for the directive
-            this.addChild = function (message) {
-                console.log('Got the message from nested directive: ' + message);
+            this.childCall = function (message) {
+                alert('Got the message from nested directive: ' + message);
             };
         }
     };
@@ -21,12 +26,22 @@ app.directive('outerDirective', function () {
 app.directive('innerDirective', function () {
     return{
         scope: {},
-        restrict: 'AE',
+        template: "<fieldset>" +
+            "<legend>inner directive</legend>"+
+            "<button ng-click='callParent()'>call parent</button>"+
+            "</fieldset>",
+        
+        controller : function($scope){          
+            $scope.callParent =  function(){
+                $scope.parentCtrl.childCall("Hi, Parent directive");
+            };
+        },
+        restrict: 'E',
         require: '^outerDirective',
-        link: function (scope, elem, attrs, controllerInstance) {
+        link: function ($scope, elem, attrs, controllerInstance) {
             //the 4th argument is the controller instance you required outerDirective
-            var message = "Hi, Parent directive";
-            controllerInstance.addChild(message);
+            $scope.parentCtrl = controllerInstance;
+            //controllerInstance.childCall(Hi, Parent directive);
         }
     };
 });
