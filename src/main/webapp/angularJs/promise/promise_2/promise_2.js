c96a0b0d42acc508acf35260a6f34cf4cc143fbb
Justin Wu <justin01.wu@gmail.com>
2016-03-09 17:56:14 -0500

    add promise2 sample

---
+++ src/main/webapp/angularJs/promise/promise_2/promise_2.js
@@ -0,0 +1,66 @@
+
+(function (angular) {
+    'use strict';
+
+    var myApp = angular.module('PersonApp', []);
+    myApp.controller('PersonCtrl', PersonController);
+    myApp.factory("personService", personService);
+
+    function personService($http, $q) {
+        return{
+            
+            getUrl: function(wrongDataFlag){
+                if (wrongDataFlag) {
+                    return "data/person_noExist.json";
+                }else{
+                    return "data/person.json";
+                }                
+            },
+            
+            getPerson: function (wrongDataFlag) {
+                
+                console.log("getting person data is called");
+                
+                var apiUrl = this.getUrl(wrongDataFlag);
+                
+                //Creating a deferred object
+                var deferred = $q.defer();
+                
+                
+                $http.get(apiUrl).success(function (data) {
+                    //Passing data to deferred's resolve function on successful completion
+                    console.log("getting person data is returned");
+                    deferred.resolve(data);
+                }).error(function () {
+                    //Sending a friendly error message in case of failure
+                    deferred.reject("An error occured while fetching person basic info");
+                });
+
+                //Returning the promise object
+                return deferred.promise;
+            }
+        };
+    }
+
+    function PersonController($scope, personService  ) {
+
+        $scope.person = {};
+        $scope.loadPerson = function (wrongDataFlag) {
+            console.log("loadPerson is called in controller");
+            $scope.person = {};
+            personService.getPerson(wrongDataFlag).then(
+                    function (data) {
+                        console.log("getting person data");
+                        $scope.person = data;
+                        $scope.errMsg = null;
+                    },
+                    function (errorMessage) {
+                        console.log(errorMessage);
+                        $scope.errMsg = errorMessage;
+                    }
+            );
+        };
+
+    }
+
+})(angular); // close anonymous function
