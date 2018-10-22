c1fec0203ce30b4e9eee99f433f4ec634258b7b1
Justin01Wu <justin01.wu@gmail.com>
2018-10-22 10:00:06 -0400

    improve first type script sample

--- src/main/webapp/ts/first.ts
+++ src/main/webapp/ts/first.ts
@@ -1,15 +1,26 @@
-class Student {
+// this is my first type script code 
+
+
+/**
+
+	Student class implements Person interface
+*/
+class Student implements Person{
     fullName: string;
     constructor(public firstName: string, public middleInitial: string, public lastName: string) {
         this.fullName = firstName + " " + middleInitial + " " + lastName;
     }
 }
 
+/**
+	my interface 
+*/
 interface Person {
     firstName: string;
     lastName: string;
 }
 
+// function to use interface
 function greeter(person : Person) {
     return "Hello, " + person.firstName + " " + person.lastName;
 }
